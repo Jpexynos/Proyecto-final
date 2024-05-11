c4ceffac61ca89240507d57868dfb108980e7c89
JuanPabloJurado <167464417+Jpexynos@users.noreply.github.com>
2024-05-11 16:46:22 -0500

    Update Administrador.java

--- src/main/java/co/edu/uniquindio/poo/Administrador.java
+++ src/main/java/co/edu/uniquindio/poo/Administrador.java
@@ -4,6 +4,6 @@ package co.edu.uniquindio.poo;
 
         public Administrador(String nombre, String apellido, String telefono, String identificacion) {
             super(nombre, apellido, telefono, identificacion);
-            //TODO Auto-generated constructor stub
+           
         }   
 }
